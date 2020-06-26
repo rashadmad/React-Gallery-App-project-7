ce5e6231ef4703cf07623d0941110b362781dc70
Rashad Madison <tap-rmadison@Rashads-Air.attlocal.net>
2020-06-25 17:33:07 -0700

    create nav component

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// import components
+import Nav from './components/Nav';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         {/* add the search bar here */}
-        {/* add the navigation here */}
+        <Nav />
       </header>
       <ul>
         {/* add the photo components here */}
---
+++ react-app/src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Nav = (props) => {
+    <nav>
+        <input type="text" placeholder="Search..">  
+        <a href="#">Cats</a> 
+        <a href="#">Dogs</a> 
+        <a href="#">Birds</a> 
+    </nav>
+}
+
+export default Nav;
