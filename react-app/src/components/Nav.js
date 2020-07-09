1c21f4d5b80180d1ce2d2ec0fab1cb842704a6cd
Rashad Madison <tap-rmadison@Rashads-Air.attlocal.net>
2020-07-09 09:29:29 -0700

    add a search event

--- react-app/src/components/Nav.js
+++ react-app/src/components/Nav.js
@@ -12,19 +12,20 @@ const searchButtons = [
 ]
 class Nav extends Component {
 
-    clickEvent = (event) => {
-        console.log("print")
+    onSearch = (event) => {
+        event.preventDefault();
+        this.props.generateGallery(this.state.searchValue);
     }
 
     render() { 
         return (
             <Route>
                 <input type="search" id="site-search" name="searchField" aria-label="Choose a topic to search for"></input>
-                <NavLink to="/search">Search</NavLink>
+                <NavLink to="/search" onClick={this.onSearch}>Search</NavLink>
                 <nav>
-                    <NavLink to={'/' + searchButtons[0]} onClick={this.clickEvent}>{searchButtons[0]}</NavLink> 
-                    <NavLink to={'/' + searchButtons[1]} onClick={this.clickEvent}>{searchButtons[1]}</NavLink> 
-                    <NavLink to={'/' + searchButtons[2]} onClick={this.clickEvent}>{searchButtons[2]}</NavLink> 
+                    <NavLink to={'/' + searchButtons[0]}>{searchButtons[0]}</NavLink> 
+                    <NavLink to={'/' + searchButtons[1]}>{searchButtons[1]}</NavLink> 
+                    <NavLink to={'/' + searchButtons[2]}>{searchButtons[2]}</NavLink> 
                 </nav> 
             </Route> 
         )
