8fb2448e1f107a4e53bd0b70a130707de8867b5e
Rashad Madison <tap-rmadison@Rashads-Air.attlocal.net>
2020-07-23 17:19:50 -0700

    fix assest issues

--- react-app/src/components/Gallery.js
+++ react-app/src/components/Gallery.js
@@ -3,15 +3,6 @@ import React, { Component } from "react";
 //import components
 import Photo from "./Photo";
 
-//displays when the request has not completed yet
-const loading = () => {
-  return <h3 className="wrapper">loading</h3>;
-}
-
-//displays when the request has not completed yet
-const requestFailed = () => {
-  return <h3 className="wrapper">That query brought back no images.</h3>;
-}
 
 class Gallery extends Component {
 
--- react-app/src/components/Search.js
+++ react-app/src/components/Search.js
@@ -3,18 +3,6 @@ import React, { Component } from "react";
 //import components
 import Gallery from "./Gallery";
 
-//<Gallery searchApi={this.searchApi} applicationState={this.props.applicationState.requestIncomplete}/>
-
-//displays when the query made in the search does not get any results
-const start = () => {
-  return <h3 className="wrapper">Search through Flickr's database for images</h3>;
-}
-
-//displays when the query made in the search does not get any results
-const queryNotFound = () => {
-  return <h3 className="wrapper">Sorry your query did not bring back any results</h3>;
-}
-
 class Search extends Component {
 
   componentDidMount(){
